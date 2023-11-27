2f115125ff9c47181b533e8cec5a6bff9031e357
delanoaugusto <delanoaugusto@gmail.com>
2023-11-27 12:06:10 -0300

    add tempo

--- src/componets/PlacarContainer.js
+++ src/componets/PlacarContainer.js
@@ -8,7 +8,10 @@ function PlacarContainer(){
 
     const [mandante, setmandante] = useState('');
     const [visitante, setvisitante] = useState('');
-    
+    const [periodo, setPeriodo] = useState(0)
+    const handleChange = (event) => {
+      setPeriodo(event.target.value)
+    }
 
     return(
         <>
@@ -34,6 +37,18 @@ function PlacarContainer(){
 
           </label>
             </div>
+
+            <div className="periodo">
+              
+                           
+        <form>
+      <select value={periodo} onChange={handleChange}>
+        <option value="1">1º Tempo</option>
+        <option value="2">2º Tempo</option>
+      </select>
+    </form>
+      
+       </div>
             </div>
         <div className="PlacarContainer">
 
@@ -41,14 +56,20 @@ function PlacarContainer(){
           
                 <Time nome={mandante}/>
             </div>
+            
             <div className="InfoPartida">
+            <div className="periodos">
+                {periodo}º Tempo 
+                </div>
                 <div className="Local">
                 Seridosão 2023
                 </div>
                 <div className="Competicao">
                 Ouro Branco/RN
                 </div>
+                
             </div>
+            
             <div>
            
           
